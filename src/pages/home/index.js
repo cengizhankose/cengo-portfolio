973e9f61c9a6b18d69163f3bc5f836c0ca7a5feb
Cengizhan KOSE <kose651@gmail.com>
2023-01-09 20:32:58 +0300

    little routing update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -39,9 +39,9 @@ export const Home = () => {
                 </h1>
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
-                  <Link to="/portfolio" className="text_2">
+                  <Link to="/about" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
-                      My Portfolio
+                      About Me
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
