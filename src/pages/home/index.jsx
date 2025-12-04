a078aff35c6ff0a1f04762dc8be1c0768138314b
cengo <kose651@gmail.com>
2025-12-04 11:31:25 +0300

    compressed photo and impove the loading of the main photo

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
+import photoImg from "../../assets/images/photo.JPG";
 
 export const Home = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <HelmetProvider>
       <section id="home" className="home">
@@ -13,12 +16,22 @@ export const Home = () => {
           <meta charSet="utf-8" />
           <title> {meta.title}</title>
           <meta name="description" content={meta.description} />
+          <link rel="preload" as="image" href={photoImg} fetchPriority="high" />
         </Helmet>
         <div className="intro_sec d-block d-lg-flex align-items-center ">
-          <div
-            className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{ backgroundImage: `url(${introdata.your_img_url})` }}
-          ></div>
+          <div className="h_bg-image order-1 order-lg-2 h-100 position-relative">
+            {!imageLoaded && <div className="img-placeholder" />}
+            <img
+              src={photoImg}
+              alt="Cengizhan KÖSE"
+              style={{
+                opacity: imageLoaded ? 1 : 0,
+              }}
+              fetchPriority="high"
+              loading="eager"
+              onLoad={() => setImageLoaded(true)}
+            />
+          </div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
